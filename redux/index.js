deddf6ddfbdc205509c85cffccb6b300786cdd8e
6thSence <d.pushkarskaya.pr@gmail.com>
2016-01-06 14:21:33 +0600

    +index js

---
+++ redux/index.js
@@ -0,0 +1,15 @@
+import 'babel-core/polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import 'todomvc-app-css/index.css'
+
+
+
+render(
+
+    <h1>Hi, im from index.js</h1>
+
+,
+  document.getElementById('root')
+)
