289fb63822131321d352e4ee22934ec81a0f21c0
6thSence <d.pushkarskaya.pr@gmail.com>
2016-01-07 14:33:08 +0600

    +last components in mainSection and actions

--- redux/components/TodoTextInput.js
+++ redux/components/TodoTextInput.js
@@ -2,36 +2,36 @@ import React, { Component, PropTypes } from 'react'
 import classnames from 'classnames'
 
 class TodoTextInput extends Component {
-	constructor(props, context) {
-		super(props, context)
-		this.state = {
-			text: this.props.text || ''
-		}
-	}
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      text: this.props.text || ''
+    }
+  }
 
-	handleSubmit(e) {
-		const text = e.target.value.trim()
-		if (e.which === 13) {
-			this.props.onSave(text) 
-			if (this.props.NewTodo) {
-				this.setState({ text: '' })
-			}
-		}
-	}
+  handleSubmit(e) {
+    const text = e.target.value.trim()
+    if (e.which === 13) {
+      this.props.onSave(text)
+      if (this.props.newTodo) {
+        this.setState({ text: '' })
+      }
+    }
+  }
 
-	handelChange(e) {
-		this.setState({ text: e.target.value })
-	}
+  handleChange(e) {
+    this.setState({ text: e.target.value })
+  }
 
-	handleBlur(e) {
-		if (!this.props.newTodo) {
-			this.props.onSave(e.target.value)
-		}
-	}
+  handleBlur(e) {
+    if (!this.props.newTodo) {
+      this.props.onSave(e.target.value)
+    }
+  }
 
-	render() {
-		return (
-			<input className={
+  render() {
+    return (
+      <input className={
         classnames({
           edit: this.props.editing,
           'new-todo': this.props.newTodo
@@ -41,10 +41,10 @@ class TodoTextInput extends Component {
         autoFocus="true"
         value={this.state.text}
         onBlur={this.handleBlur.bind(this)}
-        onChange={this.handelChange.bind(this)}
+        onChange={this.handleChange.bind(this)}
         onKeyDown={this.handleSubmit.bind(this)} />
-		)
-	}
+    )
+  }
 }
 
 TodoTextInput.propTypes = {
